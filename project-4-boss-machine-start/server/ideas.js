28a9b57a8b227a7e297c2afa844ffbc137a41357
Silvers Rayleigh <srayleighdk@gmail.com>
2022-08-11 16:13:39 -0400

    done boss-machine

---
+++ project-4-boss-machine-start/server/ideas.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const ideasRouter = express.Router();
+
+module.exports = ideasRouter;
+
+const {
+    addToDatabase,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+} = require('./db');
+
+const checkMinionDollarIdea = require('./checkMillionDollarIdea');
+
+ideasRouter.param('id', (req, res, next, id) => {
+    const idea = getFromDatabaseById('ideas', id);
+    if (idea) {
+        req.idea = idea;
+        next();
+    } else {
+        res.status(404).send();
+    }
+});
+
+ideasRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('ideas'))
+});
+
+ideasRouter.post('/', checkMinionDollarIdea, (req, res, next) => {
+    const newIdea = addToDatabase('ideas', req.body);
+    res.status(201).send(newIdea);
+});
+
+ideasRouter.get('/:id', (req, res, next) => {
+    res.send(req.body);
+});
+
+ideasRouter.put('/:id', checkMinionDollarIdea, (req, res, next) => {
+    let updateIdeaInstance = updateInstanceInDatabase('ideas', req.body);
+    res.send(updateIdeaInstance);
+});
+
+ideasRouter.delete('/:id', (req, res, next) => {
+    const deleted = deleteFromDatabasebyId('ideas', req.params.id);
+    if (deleted) {
+        res.status(204);
+    } else {
+        res.status(500);
+    }
+    res.send();
+})
